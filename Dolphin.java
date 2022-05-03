f60cf424a4a59999127eeb909c81f1f56d0b2d5a
patrick-hu7 <348688052@gapps.yrdsb.ca>
2022-05-03 10:50:13 -0400

    added wasd movement

--- Dolphin.java
+++ Dolphin.java
@@ -12,8 +12,23 @@ public class Dolphin extends Actor
      * Act - do whatever the Dolphin wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Dolphin() {
+        
+    }
+    
     public void act()
     {
-        move(1);
+        if (Greenfoot.isKeyDown("a")) {
+            move(-1);
+        }
+        else if (Greenfoot.isKeyDown("d")) {
+            move(1);
+        }
+        else if (Greenfoot.isKeyDown("w")) {
+            setLocation(getX(), getY() - 1);
+        }
+        else if (Greenfoot.isKeyDown("s")) {
+            setLocation(getX(), getY() + 1);
+        }
     }
 }
--- MyWorld.java
+++ MyWorld.java
@@ -16,6 +16,8 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);
+        Dolphin dolphin = new Dolphin();
+        addObject(dolphin, 300, 200);
     }
 }
