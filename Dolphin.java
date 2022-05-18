a948b5ed4cdd0d5f3ebbde323472a2a6a590ef01
patrick-hu7 <348688052@gapps.yrdsb.ca>
2022-05-18 10:20:33 -0400

    slowed down animation and added left facing images

--- Dolphin.java
+++ Dolphin.java
@@ -10,37 +10,55 @@ public class Dolphin extends Actor
 {        
     GreenfootSound dolphinSound = new GreenfootSound("crunch.mp3");
     GreenfootImage idle = new GreenfootImage("./dino-animation/dino0.png");
-    GreenfootImage[] animation = new GreenfootImage[6];
+    GreenfootImage[] rightImages = new GreenfootImage[6];
+    GreenfootImage[] leftImages = new GreenfootImage[6];
+
     private int animation_index = 0;
+    SimpleTimer animTimer = new SimpleTimer();
+    private boolean facingRight = true;
     
     public Dolphin() {
         setImage(idle);
         // store the sprite animation
         for (int i = 0; i < 6; i++) {
-            GreenfootImage img = new GreenfootImage("./dino-animation/dino" + i + ".png");
-            animation[i] = img;
+            rightImages[i] = new GreenfootImage("./dino-animation/dino" + i + ".png");
+            rightImages[i].scale(50, 50);
+            leftImages[i] = new GreenfootImage("./dino-animation/dino" + i + ".png");
+            leftImages[i].scale(50, 50);
+            leftImages[i].mirrorHorizontally();
         }
+        animTimer.mark();
     }
         
     public void act()
-    {           
+    {     
         checkMovement();
         checkTouching();
         animate();
     }
     
     public void animate() {
-        setImage(animation[animation_index]);
-        animation_index++;
-        animation_index %= 5;
+        if (animTimer.millisElapsed() > 500) {
+            if (facingRight) {
+                setImage(rightImages[animation_index]);
+            }
+            else {
+                setImage(leftImages[animation_index]);
+            }
+            animation_index++;
+            animation_index %= 5;
+            animTimer.mark();
+        }        
     }
     
     public void checkMovement() {
         if (Greenfoot.isKeyDown("a")) {
             move(-1);
+            facingRight = false;
         }
         else if (Greenfoot.isKeyDown("d")) {
             move(1);
+            facingRight = true;
         }
         else if (Greenfoot.isKeyDown("w")) {
             setLocation(getX(), getY() - 1);
